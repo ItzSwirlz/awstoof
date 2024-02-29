d7b39a35ddfc63b501281e94a4da5213d175b790
Joshua Peisach <itzswirlz2020@outlook.com>
2024-02-29 12:57:36 -0500

    Fire blocks: when block underneath is destroyed, estinguish itself

--- src/main/java/com/github/itzswirlz/awstoof/block/CopperFireBlock.java
+++ src/main/java/com/github/itzswirlz/awstoof/block/CopperFireBlock.java
@@ -4,12 +4,20 @@ import net.minecraft.block.AbstractFireBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.WorldAccess;
 import net.minecraft.world.WorldView;
 
 public class CopperFireBlock extends AbstractFireBlock {
     // TODO: Do we want copper fires to oxidize?
     public CopperFireBlock(Settings settings) {
         super(settings, 1.0F);
+    }
+
+    // This is deprecated (in AbstractBlock) but its needed to ensure if the block below the fire breaks, we remove it.
+    // SoulFireBlock uses it though?
+    public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
+        return this.canPlaceAt(state, world, pos) ? this.getDefaultState() : Blocks.AIR.getDefaultState();
     }
 
     @Override
